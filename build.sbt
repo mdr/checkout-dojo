b0acb97fa146ac3648a8730ca22b47868f1b8a55
Matt Russell <MattRussellUK@gmail.com>
2015-02-04 19:17:02 +0000

    Initial

---
+++ build.sbt
@@ -0,0 +1,6 @@
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.0" % "test"
+
+scalaVersion := "2.11.1"
+
+
+
---
+++ src/test/scala/dojo/CheckoutTest.scala
@@ -0,0 +1,14 @@
+package dojo
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class CheckoutTest extends FlatSpec with ShouldMatchers {
+
+  "Checkout" should "work" in {
+    
+    
+    
+  }
+  
+}
