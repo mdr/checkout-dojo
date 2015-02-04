6f1470b8fafc9a1aa74f1ca233f4437d552fb561
Matt Russell <MattRussellUK@gmail.com>
2015-02-04 19:39:59 +0000

    impl second test

--- src/main/scala/dojo/CheckoutCalculator.scala
+++ src/main/scala/dojo/CheckoutCalculator.scala
@@ -6,9 +6,15 @@ case object Beans extends Item
 case object Coke extends Item
 case object Deodorant extends Item
 
-
 object CheckoutCalculator {
 
-  def total(items: Item*): Int = 30
-  
+  def total(items: Item*): Int = items.map(rawPrice).sum
+
+  def rawPrice(item: Item): Int = item match {
+    case Apple     ⇒ 30
+    case Beans     ⇒ 50
+    case Coke      ⇒ 180
+    case Deodorant ⇒ 250
+  }
+
 }
