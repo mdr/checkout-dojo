71f0961e9141c53111edce6ee92b6dc668825410
Matt Russell <MattRussellUK@gmail.com>
2015-02-04 20:13:44 +0000

    refactor

--- src/main/scala/dojo/CheckoutCalculator.scala
+++ src/main/scala/dojo/CheckoutCalculator.scala
@@ -11,13 +11,26 @@ object CheckoutCalculator {
   def total(items: Item*): Int = items.map(rawPrice).sum - discount(items)
 
   def discount(items: Seq[Item]): Int = {
-    val itemCounts = items.groupBy { identity }.mapValues { x ⇒ x.length }
-    val appleCounts = itemCounts.getOrElse(Apple, 0)
-    val appleDiscount = 20 * (appleCounts / 4)
-    
-    val deodorantCounts = itemCounts.getOrElse(Deodorant, 0)
-    val deodorantDiscount = 50 * (deodorantCounts / 2)
-    appleDiscount + deodorantDiscount
+    val itemCounts = items.groupBy(identity).mapValues(_.length)
+    def calculateDiscount(item: Item, count: Int) = discountAmount(item) * (count / discountQuantity(item))
+    itemCounts.map { case (item, count) ⇒ calculateDiscount(item, count) }.sum
+  }
+
+  def discountBAD(items: Seq[Item]): Int =
+    items.groupBy(identity).mapValues(_.length).map { case (item, count) ⇒ discountAmount(item) * (count / discountQuantity(item)) }.sum
+
+  def discountQuantity(item: Item): Int = item match {
+    case Apple     ⇒ 4
+    case Beans     ⇒ 1
+    case Coke      ⇒ 1
+    case Deodorant ⇒ 2
+  }
+
+  def discountAmount(item: Item): Int = item match {
+    case Apple     ⇒ 20
+    case Beans     ⇒ 0
+    case Coke      ⇒ 0
+    case Deodorant ⇒ 50
   }
 
   def rawPrice(item: Item): Int = item match {
