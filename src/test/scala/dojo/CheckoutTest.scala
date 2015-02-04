e6babe0d545a8939c1f87e6c7e9a2a4000f9576b
Matt Russell <MattRussellUK@gmail.com>
2015-02-04 19:27:50 +0000

    define func

--- src/test/scala/dojo/CheckoutTest.scala
+++ src/test/scala/dojo/CheckoutTest.scala
@@ -2,14 +2,19 @@ package dojo
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import dojo.CheckoutCalculator._
+
+// Apple £0.30   4 for £1.00
+// Beans £0.50   
+// Coke  £1.80
+// Deo   £2.50   2 for £4.50
 
 class CheckoutTest extends FlatSpec with ShouldMatchers {
 
   "Checkout" should "work" in {
 
-    42 should equal (42)
-    
-    
+    total(Apple) should equal(30)
+
   }
-  
+
 }
