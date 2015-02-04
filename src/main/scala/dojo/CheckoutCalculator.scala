ec2042882b3f7db29a651a162c73df3a404bc525
Matt Russell <MattRussellUK@gmail.com>
2015-02-04 20:42:12 +0000

    tidy up

--- src/main/scala/dojo/CheckoutCalculator.scala
+++ src/main/scala/dojo/CheckoutCalculator.scala
@@ -1,11 +1,11 @@
 package dojo
 
-sealed trait Item
-case object Apple extends Item
-case object Beans extends Item
-case object Coke extends Item
-case object Deodorant extends Item
-case object Egg extends Item
+sealed trait Item { val cost: Int }
+case object Apple extends Item { val cost: Int = 30 }
+case object Beans extends Item { val cost: Int = 50 }
+case object Coke extends Item { val cost: Int = 180 }
+case object Deodorant extends Item { val cost: Int = 250 }
+case object Egg extends Item { val cost: Int = 120 }
 
 case class DiscountGroup(items: Map[Item, Int], discountAmount: Int) {
 
@@ -19,7 +19,12 @@ case class DiscountGroup(items: Map[Item, Int], discountAmount: Int) {
 
 object CheckoutCalculator {
 
-  val AllDiscounts = {
+  /**
+   * Calculate total checkout price for a collection of items
+   */
+  def total(items: Item*): Int = items.map(_.cost).sum - discount(items)
+
+  private val AllDiscounts = {
     val appleDiscount = DiscountGroup(Map(Apple -> 4), 20)
     val deodorantDiscount = DiscountGroup(Map(Deodorant -> 2), 50)
     val eggDiscount = DiscountGroup(Map(Egg -> 3), 60)
@@ -27,40 +32,15 @@ object CheckoutCalculator {
     Seq(appleDiscount, deodorantDiscount, cokeBeanDiscount, eggDiscount)
   }
 
-  def total(items: Item*): Int = items.map(basePrice).sum - discount(items)
-
-  def getDiscounts(itemCounts: Map[Item, Int]): Seq[DiscountGroup] = {
+  private def getDiscounts(itemCounts: Map[Item, Int]): Seq[DiscountGroup] = {
     for (discount ← AllDiscounts if discount.applies(itemCounts))
       return discount +: getDiscounts(discount.apply(itemCounts))
     Seq() // No discounts
   }
-  def discount(items: Seq[Item]): Int = {
+
+  private def discount(items: Seq[Item]): Int = {
     val itemCounts: Map[Item, Int] = items.groupBy(identity).mapValues(_.length)
     getDiscounts(itemCounts).map(_.discountAmount).sum
-  }
-
-  def discountQuantity(item: Item): Int = item match {
-    case Apple     ⇒ 4
-    case Beans     ⇒ 1
-    case Coke      ⇒ 1
-    case Deodorant ⇒ 2
-    case Egg       ⇒ 3
-  }
-
-  def discountAmount(item: Item): Int = item match {
-    case Apple     ⇒ 20
-    case Beans     ⇒ 0
-    case Coke      ⇒ 0
-    case Deodorant ⇒ 50
-    case Egg       ⇒ 60
-  }
-
-  def basePrice(item: Item): Int = item match {
-    case Apple     ⇒ 30
-    case Beans     ⇒ 50
-    case Coke      ⇒ 180
-    case Deodorant ⇒ 250
-    case Egg       ⇒ 120
   }
 
 }
