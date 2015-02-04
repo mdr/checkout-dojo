6d6949ac2249a696a0978730f154f05f27e6f011
Matt Russell <MattRussellUK@gmail.com>
2015-02-04 19:28:26 +0000

    added noob file

---
+++ src/main/scala/dojo/CheckoutCalculator.scala
@@ -0,0 +1,14 @@
+package dojo
+
+sealed trait Item
+case object Apple extends Item
+case object Beans extends Item
+case object Coke extends Item
+case object Deodorant extends Item
+
+
+object CheckoutCalculator {
+
+  def total(items: Item*): Int = ???
+  
+}
